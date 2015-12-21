507c4f89df2485df48ec35dc78b10591d140bdd1
danwoo <danwoopark@gmail.com>
2015-12-21 22:11:21 +0900

    FAB 스크롤시 사라지게 변경

--- app/src/main/java/hci/com/vocaagent/Animation/ScrollAwareFABBehavior.java
+++ app/src/main/java/hci/com/vocaagent/Animation/ScrollAwareFABBehavior.java
@@ -3,6 +3,7 @@ package hci.com.vocaagent.Animation;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.view.ViewCompat;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -11,6 +12,14 @@ public class ScrollAwareFABBehavior extends FloatingActionButton.Behavior {
 
     public ScrollAwareFABBehavior(Context context, AttributeSet attrs) {
         super();
+    }
+
+    @Override
+    public boolean onStartNestedScroll(CoordinatorLayout coordinatorLayout,
+                                       FloatingActionButton child, View directTargetChild, View target, int nestedScrollAxes) {
+        return nestedScrollAxes == ViewCompat.SCROLL_AXIS_VERTICAL ||
+                super.onStartNestedScroll(coordinatorLayout, child, directTargetChild, target,
+                        nestedScrollAxes);
     }
 
     @Override
